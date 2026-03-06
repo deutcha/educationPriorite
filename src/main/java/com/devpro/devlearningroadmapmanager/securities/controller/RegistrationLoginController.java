2bed7e95dafd7043e9227b951c4fd38c357d71c2
Deutcha yvan <deutchayvan36@gmail.com>
2026-03-06 14:40:45 +0100

    update du app

--- src/main/java/com/devpro/devlearningroadmapmanager/securities/controller/RegistrationLoginController.java
+++ src/main/java/com/devpro/devlearningroadmapmanager/securities/controller/RegistrationLoginController.java
@@ -12,7 +12,6 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor
 public class RegistrationLoginController {
 
